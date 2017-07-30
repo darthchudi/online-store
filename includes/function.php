8de8db55c3b70378b6280a842644789f9cb820fd
darthchudi <chukwudioranu@ymail.com>
2017-07-30 14:39:14 +0100

    Added new functions

--- includes/function.php
+++ includes/function.php
@@ -46,8 +46,8 @@
 	}
 
 	function authenticate(){
-		if(!isset($_SESSION['id'])){
-			header("Location:index.php");
+		if(!isset($_SESSION['admin_id'])){
+			header("Location:admin_index.php");
 		}
 	}
 
@@ -58,14 +58,28 @@
 	}
 
 	function logout(){
-		unset($_SESSION['id']);
-		header("index.php");
+		unset($_SESSION['admin_id']);
+		header("Location: admin_index.php");
 	}
 
 	function logoutCustomer(){
 		unset($_SESSION['loginId']);
 		header("Location: index.php");
 	}
+
+	function adminRedirect(){
+		if(isset($_SESSION['admin_id'])){
+			header("Location: admin_home.php");
+		}
+	}
+
+	function customerRedirect(){
+		if(isset($_SESSION['loginId'])){
+			header("Location: catalogue.php");
+		}
+	}
+
+
 
 	function fetchAdminDetails($dbconn, $admin_id){
 		$stmt=$dbconn->prepare("SELECT * FROM admin WHERE admin_id=:id");
@@ -126,25 +140,55 @@
 		$stmt->execute();
 		$rows=$stmt->fetchAll();
 
-		$stmt=$dbconn->prepare("INSERT INTO cart(book_name, author_name, book_category, book_price, customer_name, customer_id) 
-								VALUES(:book_name, :author_name, :book_category, :book_price, :customer_name, :customer_id)");
-		$data=[":book_name"=>$rows[0]['book_name'],
+
+		$stmt=$dbconn->prepare("SELECT book_name, quantity FROM cart WHERE book_id=:book_id AND customer_id=:customer_id");
+		$check=[":book_id"=>$id, ":customer_id"=>$customer_id];
+		$stmt->execute($check);
+		$book_details=$stmt->fetch(PDO::FETCH_ASSOC);
+
+		if($stmt->rowCount() > 0){
+			$quantity=$book_details['quantity'];
+			$quantity+=1;
+
+			$stmt=$dbconn->prepare("UPDATE cart SET quantity=:quantity WHERE book_name=:book_name AND customer_id=:customer_id");
+			$data=[ ":quantity"=>$quantity, ":book_name"=>$rows[0]['book_name'], ":customer_id"=>$customer_id];
+			$stmt->execute($data);
+			header("Location:catalogue.php");
+			return true;
+		}
+
+		else{
+			$quantity=1;
+			$stmt=$dbconn->prepare("INSERT INTO cart(book_name, book_id, author_name, book_category, book_price, quantity, customer_name, customer_id) 
+								VALUES(:book_name, :book_id, :author_name, :book_category, :book_price, :quantity,  :customer_name, :customer_id)");
+			$data=[":book_name"=>$rows[0]['book_name'],
+				":book_id"=>$id,
 				":author_name"=>$rows[0]['author_name'],
 				":book_category"=>$rows[0]['book_category'],
 				":book_price"=>$rows[0]['book_price'],
+				":quantity"=>$quantity,
 				":customer_name"=>$customerFullName,
 				":customer_id"=>$customer_id
-		];
-		$stmt->execute($data);
-		return true;
+			];
+			$stmt->execute($data);
+			header("Location:catalogue.php");
+			return true;
+		}
+
+		
 	}
 
 	function fetchNumItemsInCart($dbconn, $id){
-		$stmt=$dbconn->prepare("SELECT * FROM cart WHERE customer_id= :id");
+		$stmt=$dbconn->prepare("SELECT quantity FROM cart WHERE customer_id= :id");
 		$stmt->bindParam(":id", $id);
 		$stmt->execute();
-		$num=$stmt->rowCount();
-		return $num;
+		$count=$stmt->rowCount()-1;
+		$stuff=$stmt->fetchAll(PDO::FETCH_COLUMN);
+		$items=0;
+		for ($i=0; $i<=$count; $i++){
+			$items+=$stuff[$i];
+		}
+		return $items;
 	}
 
 	function customerRegister($dbconn, $input){
@@ -198,6 +242,7 @@
 		$stmt=$dbconn->prepare("DELETE FROM cart WHERE order_id=:order_id");
 		$stmt->bindParam(":order_id", $orderID);
 		$stmt->execute();
+		header("Location:view_cart.php");
 		return true;
 	}
 
@@ -208,5 +253,87 @@
 		header("Location: view_cart.php");
 		return true;
 	}
+
+	function deleteBook($dbconn, $book_id, $book_name){
+		$stmt=$dbconn->prepare("DELETE FROM books WHERE book_id=:book_id");
+		$stmt->bindParam(":book_id", $book_id);
+		$stmt->execute();
+
+
+		$stmt=$dbconn->prepare("DELETE FROM cart WHERE book_name=:book_name");
+		$stmt->bindParam(":book_name", $book_name);
+		$stmt->execute();
+		header("Location:view_books.php");
+	}
+
+	function fetchBookDetails($dbconn, $book_id){
+		$stmt=$dbconn->prepare("SELECT * FROM books WHERE book_id=:book_id");
+		$stmt->bindParam(":book_id", $book_id);
+		$stmt->execute();
+		$rows=$stmt->fetch(PDO::FETCH_ASSOC);
+		return $rows;
+	}
+
+	function editBook($dbconn, $input, $book_id){
+		$stmt=$dbconn->prepare("UPDATE books SET book_name=:book_name, book_category=:book_category, author_name=:author_name, year_of_release=:year, book_price=:book_price  
+			WHERE book_id=:book_id");
+		$data=[":book_name"=>$input['book_name'],
+			  	":book_category"=>$input['book_category'],
+			  	":author_name"=>$input['author_name'],
+			  	":year"=>$input['year'],
+			  	":book_price"=>$input['book_price'],
+			  	":book_id"=>$book_id
+		];
+		$stmt->execute($data);
+		return true;
+	}
+
+	function increaseQuantity($dbconn, $id){
+		$stmt=$dbconn->prepare("SELECT quantity FROM cart WHERE order_id=:id");
+		$stmt->bindParam(":id", $id);
+		$stmt->execute();
+		$row=$stmt->fetch(PDO::FETCH_ASSOC);
+		$quantity=$row['quantity'];
+
+		$quantity+=1;
+
+		$stmt=$dbconn->prepare("UPDATE cart SET quantity=:quantity WHERE order_id=:order_id");
+		$data=[":quantity"=>$quantity, ":order_id"=>$id];
+		$stmt->execute($data);
+
+		header("Location:view_cart.php");
+	}
+
+	function decreaseQuantity($dbconn, $id){
+		$stmt=$dbconn->prepare("SELECT quantity FROM cart WHERE order_id=:id");
+		$stmt->bindParam(":id", $id);
+		$stmt->execute();
+		$row=$stmt->fetch(PDO::FETCH_ASSOC);
+		$quantity=$row['quantity'];
+
+		$quantity-=1;
+
+		$stmt=$dbconn->prepare("UPDATE cart SET quantity=:quantity WHERE order_id=:order_id");
+		$data=[":quantity"=>$quantity, ":order_id"=>$id];
+		$stmt->execute($data);
+
+		header("Location:view_cart.php");
+	}
+
+	function emptyBook($dbconn, $customer_id){
+		$stmt=$dbconn->prepare("SELECT order_id, quantity FROM cart WHERE customer_id=:customer_id ");
+		$stmt->bindParam(":customer_id", $customer_id);
+		$stmt->execute();
+		$num_of_rows=$stmt->rowCount() -1 ;
+		$rows=$stmt->fetchAll();
+
+		for($i=0; $i<=$num_of_rows; $i++){
+			if($rows[$i]['quantity'] ==0){
+				deleteFromCart($dbconn, $rows[$i]['order_id']);
+			}
+		}
+	}
+
+
 
 ?>
